fba87144d3e31996d35cdd1d3243278631c31835
phetploy <st.phetploy@gmail.com>
2024-09-24 22:33:50 +0700

    :sparkles: initialize payment module

--- modules/payment/paymentUsecase/paymentUsecase.go
+++ modules/payment/paymentUsecase/paymentUsecase.go
@@ -1,1 +1,17 @@
-package paymentusecase
+package paymentUsecase
+
+import "github.com/phetployst/sekai-shop-microservices/modules/payment/paymentRepository"
+
+type (
+	PaymentUsecaseService interface{}
+
+	paymentUsecase struct {
+		paymentRepository paymentRepository.PaymentRepositoryService
+	}
+)
+
+func NewPaymentUsecase(paymentRepository paymentRepository.PaymentRepositoryService) PaymentUsecaseService {
+	return &paymentUsecase{
+		paymentRepository: paymentRepository,
+	}
+}
