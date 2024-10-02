dccb2aa79f7827cf67f70c1985259723eea488b7
phetploy <st.phetploy@gmail.com>
2024-10-02 08:05:57 +0700

    :technologist: initialize protobuf for authentication module

--- modules/auth/authHandler/authGrpcHandler.go
+++ modules/auth/authHandler/authGrpcHandler.go
@@ -1,11 +1,27 @@
 package authHandler
 
-import "github.com/phetployst/sekai-shop-microservices/modules/auth/authUsecase"
+import (
+	"context"
+
+	"github.com/phetployst/sekai-shop-microservices/modules/auth/authPb"
+	"github.com/phetployst/sekai-shop-microservices/modules/auth/authUsecase"
+)
 
 type authGrpcHandler struct {
+	authPb.UnimplementedAuthGrpcServiceServer
 	authUsecase authUsecase.AuthUsecaseService
 }
 
 func NewAuthGrpcHandler(authUsecase authUsecase.AuthUsecaseService) authUsecase.AuthUsecaseService {
-	return &authGrpcHandler{authUsecase: authUsecase}
+	return &authGrpcHandler{
+		authUsecase: authUsecase,
+	}
+}
+
+func (g *authGrpcHandler) AccessTokenSearch(ctx context.Context, req *authPb.AccessTokenSearchReq) (*authPb.AccessTokenSearchRes, error) {
+	return nil, nil
+}
+
+func (g *authGrpcHandler) RolesCount(ctx context.Context, req *authPb.RolesCountReq) (*authPb.RolesCountRes, error) {
+	return nil, nil
 }
