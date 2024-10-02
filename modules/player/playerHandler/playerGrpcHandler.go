85c3254f656c50b8eb9529aa3e042847d7c474e8
phetploy <st.phetploy@gmail.com>
2024-10-02 09:43:33 +0700

    :technologist: initialize Protobuf for Player Module

--- modules/player/playerHandler/playerGrpcHandler.go
+++ modules/player/playerHandler/playerGrpcHandler.go
@@ -1,9 +1,15 @@
 package playerHandler
 
-import "github.com/phetployst/sekai-shop-microservices/modules/player/playerUsecase"
+import (
+	"context"
+
+	"github.com/phetployst/sekai-shop-microservices/modules/player/playerPb"
+	"github.com/phetployst/sekai-shop-microservices/modules/player/playerUsecase"
+)
 
 type (
 	playerGrpcHandler struct {
+		playerPb.UnimplementedPlayerGrpcServiceServer
 		playerUsecase playerUsecase.PlayerUsecaseService
 	}
 )
@@ -12,4 +18,16 @@ func NewPlayerGrpcHandler(playerUsecase playerUsecase.PlayerUsecaseService) *pla
 	return &playerGrpcHandler{
 		playerUsecase: playerUsecase,
 	}
+}
+
+func (g *playerGrpcHandler) CredentialSearch(ctx context.Context, req *playerPb.CredentialSearchReq) (*playerPb.PlayerProfile, error) {
+	return nil, nil
+}
+
+func (g *playerGrpcHandler) FindOnePlayerProfileToRefresh(ctx context.Context, req *playerPb.FindOnePlayerProfileToRefreshReq) (*playerPb.PlayerProfile, error) {
+	return nil, nil
+}
+
+func (g *playerGrpcHandler) GetPlayerSavingAccount(ctx context.Context, req *playerPb.GetPlayerSavingAccountReq) (*playerPb.GetPlayerSavingAccountRes, error) {
+	return nil, nil
 }
