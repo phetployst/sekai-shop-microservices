31b0f800664c735d96b47d2ee3a83462e77f1b19
phetploy <st.phetploy@gmail.com>
2024-10-09 20:21:43 +0700

    :package: implement create access token, refresh token, and API key factories

--- pkg/jwtauth/jwtauth.go
+++ pkg/jwtauth/jwtauth.go
@@ -1,6 +1,9 @@
 package jwtauth
 
 import (
+	"math"
+	"time"
+
 	"github.com/golang-jwt/jwt/v5"
 )
 
@@ -33,4 +36,96 @@ func (a *authConcrete) SignToken() string {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, a.Claims)
 	ss, _ := token.SignedString(a.Secret)
 	return ss
+}
+
+func now() time.Time {
+	loc, _ := time.LoadLocation("Asia/Bangkok")
+	return time.Now().In(loc)
+}
+
+// Note that: t is a second unit
+func jwtTimeDurationCal(t int64) *jwt.NumericDate {
+	return jwt.NewNumericDate(now().Add(time.Duration(t * int64(math.Pow10(9)))))
+}
+
+func jwtTimeRepeatAdapter(t int64) *jwt.NumericDate {
+	return jwt.NewNumericDate(time.Unix(t, 0))
+}
+
+func NewAccessToken(secret string, expiredAt int64, claims *Claims) AuthFactory {
+	return &accessToken{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claims: claims,
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "hellosekai.com",
+					Subject:   "access-token",
+					Audience:  []string{"hellosekai.com"},
+					ExpiresAt: jwtTimeDurationCal(expiredAt),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
+}
+
+func NewRefreshToken(secret string, expiredAt int64, claims *Claims) AuthFactory {
+	return &refreshToken{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claims: claims,
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "hellosekai.com",
+					Subject:   "refresh-token",
+					Audience:  []string{"hellosekai.com"},
+					ExpiresAt: jwtTimeDurationCal(expiredAt),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
+}
+
+func ReloadToken(secret string, expiredAt int64, claims *Claims) string {
+	obj := &refreshToken{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claims: claims,
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "hellosekai.com",
+					Subject:   "refresh-token",
+					Audience:  []string{"hellosekai.com"},
+					ExpiresAt: jwtTimeRepeatAdapter(expiredAt),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
+
+	return obj.SignToken()
+}
+
+func NewApiKey(secret string) AuthFactory {
+	return &apiKey{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claims: &Claims{},
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "hellosekai.com",
+					Subject:   "api-key",
+					Audience:  []string{"hellosekai.com"},
+					ExpiresAt: jwtTimeDurationCal(31560000),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
 }
