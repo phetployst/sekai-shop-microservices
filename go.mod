41cd0ea2129680c1179bfe5c17c744700f5611fd
phetploy <st.phetploy@gmail.com>
2024-10-09 19:59:57 +0700

    :package: initialize JWT authentication package

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.1
 
 require (
 	github.com/go-playground/validator/v10 v10.22.1
+	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.12.0
 	go.mongodb.org/mongo-driver v1.17.0
---
+++ pkg/jwtauth/jwtauth.go
@@ -0,0 +1,36 @@
+package jwtauth
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type (
+	AuthFactory interface {
+		SignToken() string
+	}
+
+	Claims struct {
+		PlayerId string `json:"player_id"`
+		RoleCode int    `json:"role_code"`
+	}
+
+	AuthMapClaims struct {
+		*Claims
+		jwt.RegisteredClaims
+	}
+
+	authConcrete struct {
+		Secret []byte
+		Claims *AuthMapClaims `json:"claims"`
+	}
+
+	accessToken  struct{ *authConcrete }
+	refreshToken struct{ *authConcrete }
+	apiKey       struct{ *authConcrete }
+)
+
+func (a *authConcrete) SignToken() string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, a.Claims)
+	ss, _ := token.SignedString(a.Secret)
+	return ss
+}
