75b6adc4f00ce9a46e1cfd356cbc3d8b8a6cb5fb
phetploy <st.phetploy@gmail.com>
2024-10-12 02:38:02 +0700

    :sparkles: implement login function for user authentication

--- modules/auth/authRepository/authRepository.go
+++ modules/auth/authRepository/authRepository.go
@@ -6,14 +6,25 @@ import (
 	"log"
 	"time"
 
+	"github.com/phetployst/sekai-shop-microservices/config"
+	"github.com/phetployst/sekai-shop-microservices/modules/auth"
 	"github.com/phetployst/sekai-shop-microservices/modules/player/playerPb"
 	"github.com/phetployst/sekai-shop-microservices/pkg/grpccon"
 	"github.com/phetployst/sekai-shop-microservices/pkg/jwtauth"
+	"github.com/phetployst/sekai-shop-microservices/pkg/utils"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type (
-	AuthRepositoryService interface{}
+	AuthRepositoryService interface {
+		CredentialSearch(pctx context.Context, grpcUrl string, req *playerPb.CredentialSearchReq) (*playerPb.PlayerProfile, error)
+		InsertOnePlayerCredential(pctx context.Context, req *auth.Credential) (primitive.ObjectID, error)
+		FindOnePlayerCredential(pctx context.Context, credentialId string) (*auth.Credential, error)
+		AccessToken(cfg *config.Config, claims *jwtauth.Claims) string
+		RefreshToken(cfg *config.Config, claims *jwtauth.Claims) string
+	}
 
 	authRepository struct {
 		db *mongo.Client
@@ -46,4 +57,54 @@ func (r *authRepository) CredentialSearch(pctx context.Context, grpcUrl string, 
 	}
 
 	return result, nil
+}
+
+func (r *authRepository) InsertOnePlayerCredential(pctx context.Context, req *auth.Credential) (primitive.ObjectID, error) {
+	ctx, cancel := context.WithTimeout(pctx, 10*time.Second)
+	defer cancel()
+
+	db := r.authDbConn(ctx)
+	col := db.Collection("auth")
+
+	req.CreatedAt = utils.LocalTime()
+	req.UpdatedAt = utils.LocalTime()
+
+	result, err := col.InsertOne(ctx, req)
+	if err != nil {
+		log.Printf("Error: InsertOnePlayerCredential failed: %s", err.Error())
+		return primitive.NilObjectID, errors.New("error: insert one player credential failed")
+	}
+
+	return result.InsertedID.(primitive.ObjectID), nil
+}
+
+func (r *authRepository) FindOnePlayerCredential(pctx context.Context, credentialId string) (*auth.Credential, error) {
+	ctx, cancel := context.WithTimeout(pctx, 10*time.Second)
+	defer cancel()
+
+	db := r.authDbConn(ctx)
+	col := db.Collection("auth")
+
+	result := new(auth.Credential)
+
+	if err := col.FindOne(ctx, bson.M{"_id": utils.ConvertToObjectId(credentialId)}).Decode(result); err != nil {
+		log.Printf("Error: FindOnePlayerCredential failed: %s", err.Error())
+		return nil, errors.New("error: find one player credential failed")
+	}
+
+	return result, nil
+}
+
+func (r *authRepository) AccessToken(cfg *config.Config, claims *jwtauth.Claims) string {
+	return jwtauth.NewAccessToken(cfg.Jwt.AccessSecretKey, cfg.Jwt.AccessDuration, &jwtauth.Claims{
+		PlayerId: claims.PlayerId,
+		RoleCode: int(claims.RoleCode),
+	}).SignToken()
+}
+
+func (r *authRepository) RefreshToken(cfg *config.Config, claims *jwtauth.Claims) string {
+	return jwtauth.NewRefreshToken(cfg.Jwt.RefreshSecretKey, cfg.Jwt.RefreshDuration, &jwtauth.Claims{
+		PlayerId: claims.PlayerId,
+		RoleCode: int(claims.RoleCode),
+	}).SignToken()
 }
--- modules/auth/authUsecase/authUsecase.go
+++ modules/auth/authUsecase/authUsecase.go
@@ -1,9 +1,22 @@
 package authUsecase
 
-import "github.com/phetployst/sekai-shop-microservices/modules/auth/authRepository"
+import (
+	"context"
+	"time"
+
+	"github.com/phetployst/sekai-shop-microservices/config"
+	"github.com/phetployst/sekai-shop-microservices/modules/auth"
+	"github.com/phetployst/sekai-shop-microservices/modules/auth/authRepository"
+	"github.com/phetployst/sekai-shop-microservices/modules/player"
+	"github.com/phetployst/sekai-shop-microservices/modules/player/playerPb"
+	"github.com/phetployst/sekai-shop-microservices/pkg/jwtauth"
+	"github.com/phetployst/sekai-shop-microservices/pkg/utils"
+)
 
 type (
-	AuthUsecaseService interface{}
+	AuthUsecaseService interface {
+		Login(pctx context.Context, cfg *config.Config, req *auth.PlayerLoginReq) (*auth.ProfileIntercepter, error)
+	}
 
 	authUsecase struct {
 		authRepository authRepository.AuthRepositoryService
@@ -12,4 +25,59 @@ type (
 
 func NewAuthUsecase(authRepository authRepository.AuthRepositoryService) AuthUsecaseService {
 	return &authUsecase{authRepository: authRepository}
+}
+
+func (u *authUsecase) Login(pctx context.Context, cfg *config.Config, req *auth.PlayerLoginReq) (*auth.ProfileIntercepter, error) {
+	profile, err := u.authRepository.CredentialSearch(pctx, cfg.Grpc.PlayerUrl, &playerPb.CredentialSearchReq{
+		Email:    req.Email,
+		Password: req.Password,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	profile.Id = "player:" + profile.Id
+
+	accessToken := u.authRepository.AccessToken(cfg, &jwtauth.Claims{
+		PlayerId: profile.Id,
+		RoleCode: int(profile.RoleCode),
+	})
+
+	refreshToken := u.authRepository.RefreshToken(cfg, &jwtauth.Claims{
+		PlayerId: profile.Id,
+		RoleCode: int(profile.RoleCode),
+	})
+
+	credentialId, err := u.authRepository.InsertOnePlayerCredential(pctx, &auth.Credential{
+		PlayerId:     profile.Id,
+		RoleCode:     int(profile.RoleCode),
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	})
+
+	credential, err := u.authRepository.FindOnePlayerCredential(pctx, credentialId.Hex())
+	if err != nil {
+		return nil, err
+	}
+
+	loc, _ := time.LoadLocation("Asia/Bangkok")
+
+	return &auth.ProfileIntercepter{
+		PlayerProfile: &player.PlayerProfile{
+			Id:        profile.Id,
+			Email:     profile.Email,
+			Username:  profile.Username,
+			CreatedAt: utils.ConvertStringTimeToTime(profile.CreatedAt).In(loc),
+			UpdatedAt: utils.ConvertStringTimeToTime(profile.UpdatedAt).In(loc),
+		},
+		Credential: &auth.CredentialRes{
+			Id:           credential.Id.Hex(),
+			PlayerId:     credential.PlayerId,
+			RoleCode:     credential.RoleCode,
+			AccessToken:  credential.AccessToken,
+			RefreshToken: credential.RefreshToken,
+			CreatedAt:    credential.CreatedAt.In(loc),
+			UpdatedAt:    credential.UpdatedAt.In(loc),
+		},
+	}, nil
 }
