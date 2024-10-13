452c14046b33e7d0f3eecf4153f832e6e1e6223f
phetploy <st.phetploy@gmail.com>
2024-10-14 04:23:39 +0700

    :sparkles: implement function to retrieve item prices

--- modules/payment/paymentRepository/paymentRepository.go
+++ modules/payment/paymentRepository/paymentRepository.go
@@ -2,12 +2,20 @@ package paymentRepository
 
 import (
 	"context"
+	"errors"
+	"log"
+	"time"
 
+	"github.com/phetployst/sekai-shop-microservices/modules/item/itemPb"
+	"github.com/phetployst/sekai-shop-microservices/pkg/grpccon"
+	"github.com/phetployst/sekai-shop-microservices/pkg/jwtauth"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type (
-	PaymentRepositoryService interface{}
+	PaymentRepositoryService interface {
+		FindItemsInIds(pctx context.Context, grpcUrl string, req *itemPb.FindItemsInIdsReq) (*itemPb.FindItemsInIdsRes, error)
+	}
 
 	paymentRepository struct {
 		db *mongo.Client
@@ -20,4 +28,34 @@ func NewPaymentRepository(db *mongo.Client) PaymentRepositoryService {
 
 func (r *paymentRepository) paymentDbConn(pctx context.Context) *mongo.Database {
 	return r.db.Database("payment_db")
+}
+
+func (r *paymentRepository) FindItemsInIds(pctx context.Context, grpcUrl string, req *itemPb.FindItemsInIdsReq) (*itemPb.FindItemsInIdsRes, error) {
+	ctx, cancel := context.WithTimeout(pctx, 30*time.Second)
+	defer cancel()
+
+	jwtauth.SetApiKeyInContext(&ctx)
+	conn, err := grpccon.NewGrpcClient(grpcUrl)
+	if err != nil {
+		log.Printf("Error: gRPC connection failed: %s", err.Error())
+		return nil, errors.New("error: gRPC connection failed")
+	}
+
+	result, err := conn.Item().FindItemsInIds(ctx, req)
+	if err != nil {
+		log.Printf("Error: FindItemsInIds failed: %s", err.Error())
+		return nil, errors.New("error: items not found")
+	}
+
+	if result == nil {
+		log.Printf("Error: FindItemsInIds failed: %s", err.Error())
+		return nil, errors.New("error: items not found")
+	}
+
+	if len(result.Items) == 0 {
+		log.Printf("Error: FindItemsInIds failed: %s", err.Error())
+		return nil, errors.New("error: items not found")
+	}
+
+	return result, nil
 }
